a80ca51f9f8ceb8567469e090162dbb17dcbcce1
Kudz Murefu <kudzmurefu@gmail.com>
2020-06-27 03:03:21 -0400

    1.0

---
+++ build/precache-manifest.b801f3c2e9f7993d842baaced9500ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3fba59fc784cd6d52f9c1c65b403bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768880119bc0e3dbe561",
+    "url": "/static/css/main.2160eee6.chunk.css"
+  },
+  {
+    "revision": "7d53d21b98ab8b5dbc78",
+    "url": "/static/js/2.df3d13cf.chunk.js"
+  },
+  {
+    "revision": "ca3927bb7ee98f04bdf704006a273daf",
+    "url": "/static/js/2.df3d13cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "768880119bc0e3dbe561",
+    "url": "/static/js/main.b7e1352e.chunk.js"
+  },
+  {
+    "revision": "3be30db842139dcaffbb",
+    "url": "/static/js/runtime-main.98ab045b.js"
+  }
+]);
